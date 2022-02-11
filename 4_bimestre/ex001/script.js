b01ae141577c91200cef5f997474099ce64d7c67
Marcus Cauê <cauemarcuscauefb@gmail.com>
2022-02-11 11:37:17 -0300

    Corrigindo o problema de múltiplos resultados
    A correção consiste de ter somente uma <section> contendo o valor final do prato após o usuário clicar no botão "Calcular".

--- 4_bimestre/ex001/script.js
+++ 4_bimestre/ex001/script.js
@@ -1,24 +1,22 @@
-/* Dúvida: 
-
-Professor, tem alguma forma deu restringir o uso do botão a somente um click?
-Digo isso porque toda vez que o usuário clica no botão "Calcular", aparece mais uma <section> contendo o valor total a pagar e eu gostaria que ela só aparecesse uma vez.
-
-*/
-
 function calcular() {
     var peso = document.getElementById("pesoAlmoco")
     peso = parseFloat(peso.value)
     var desconto = document.getElementById("desconto")
     desconto = parseInt(desconto.value)
 
-    /* Multiplicando o valor do peso lido pelo preço do quilo */
-    var valorPeso = (peso / 1000) * 44.5
-    /* Aplicando o desconto sobre o valor obtido */
-    var valorFinal = valorPeso - (valorPeso * (desconto / 100))
+    if (desconto < 0 || desconto > 100) {
+        window.alert("Valor do desconto inválido. Insira um valor entre 0 e 100!")
+    } else if (peso < 1) {
+        window.alert("Valor do peso inválido. Insira um valor maior que 1!")
+    } else {
+        /* Multiplicando o valor do peso lido pelo preço do quilo */
+        var valorPeso = (peso / 1000) * 44.5
+        /* Aplicando o desconto sobre o valor obtido */
+        var valorFinal = valorPeso - (valorPeso * (desconto / 100))
 
-    /* Variável que vai permitir inserir uma nova <section> contendo o valor final dentro da tag <main> */
-    var tagMain = document.getElementsByTagName("main")
+        /* Variável que vai permitir modificar o conteúdo da tag <section id="resultado"> */
+        var sectionResultado = document.querySelector("section#resultado")
 
-    /* Esses espaços em branco representam os tabs para deixar o novo bloco de conteúdo a ser inserido também identado */
-    tagMain[0].innerHTML += `\n    <section id="resultado">\n        <p>Valor total a pagar: R$${valorFinal.toFixed(2)}</p>\n    </section>`
+        sectionResultado.innerHTML = `<p>Valor total a pagar: R$${valorFinal.toFixed(2)}</p>`
+    }
 }
