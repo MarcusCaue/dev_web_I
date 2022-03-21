d5b401a9ae1bde7eca997a1f0a1eec50806e0c7f
Marcus Cauê <cauemarcuscauefb@gmail.com>
2022-03-20 23:18:40 -0300

    Exercício final concluído

--- 4_bimestre/ex003/script.js
+++ 4_bimestre/ex003/script.js
@@ -1,28 +1,32 @@
-function entrou() {
-    var title = document.querySelector("h2#mensagemOferta");
+function mostraMensagem() {
+    let title = document.querySelector("h2#mensagemOferta");
     title.innerHTML = "Aqui você encontrará as melhores ofertas!"
     title.style.filter = "drop-shadow(0px 0px 9px)";
 }
 
-function saiu() {
-    var title = document.querySelector("h2#mensagemOferta");
-    title.innerHTML = ""
+function fechaMensagem() {
+    let title = document.querySelector("h2#mensagemOferta");
+    title.innerHTML = "";
 }
-
 
 function acessar(link) {
     window.open(link);
 }
 
 function validaForm() {
-    var dadoTelefone = document.querySelector("input#tel").value;
+    let dadoTelefone = document.querySelector("input#tel").value;
 
     if (dadoTelefone == '') {
         window.alert("Digite um dado! O campo não pode ficar vazio.")
         return false;
-    } else {
+    } 
+    else {
+        let mensagem = document.querySelector("textarea#mensagemEmail")
+        let telefone = document.querySelector("input#tel").value;
+
+        mensagem.innerHTML = `Telefone informado no nosso site: ${telefone}`
+        
+        window.alert("Telefone enviado com sucesso! Confira o seu email! :)")
         return true;
     }
-
-    
 }
